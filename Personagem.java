e7557ae4a712d445dc780f702be0411ccbb7dec5
Mateus Cruz <profissionalmateuscruz@gmail.com>
2025-04-02 19:13:16 -0300

    feat: Characters Classes

---
+++ Personagem.java
@@ -0,0 +1,54 @@
+public class Personagem {
+    private String nome;
+    private int vida;
+    private int ataque;
+    private int defesa;
+
+    public Personagem(String nome, int vida, int ataque, int defesa) {
+        this.nome = nome;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getDefesa() {
+        return defesa;
+    }
+
+    public void setDefesa(int defesa) {
+        this.defesa = defesa;
+    }
+
+    public boolean estaVivo() {
+        return vida > 0;
+    }
+
+    public void receberDano(int dano) {
+        this.vida = Math.max(0, this.vida - dano);
+    }
+} 
